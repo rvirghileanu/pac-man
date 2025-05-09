46567e0ed447214b5eaffb937b193328a93c7700
rvirghileanu <roberta.virghileanu2005@gmail.com>
2025-05-09 18:28:11 +0300

    Delete POO_Proiect2_RobertaVirghileanu directory

--- POO_Proiect2_RobertaVirghileanu/include/mapLoadException.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "GameException.h"
-
-class MapLoadException : public GameException {
-public:
-    explicit MapLoadException(const std::string& msg)
-        : GameException("MapLoadException: " + msg) {}
-};
